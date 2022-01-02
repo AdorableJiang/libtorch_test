a271a26f600fc1b1d9985222824eb96700cf9ea4
quan <quan@ubuntu.dell>
2022-01-02 15:36:24 +0900

    human-friendly output

--- libtorch_test.cpp
+++ libtorch_test.cpp
@@ -7,10 +7,24 @@ using namespace std;
 int main() {
     // 分别打印CUDA、cuDNN是否可用以及可用的CUDA设备个数
     // 可以发现函数名是和PyTorch里一样的
-    cout << torch::cuda::is_available() << endl;
-    cout << torch::cuda::cudnn_is_available() << endl;
-    cout << torch::cuda::device_count() << endl;
-    cout << "PyTorch version: "<< TORCH_VERSION_MAJOR<<"."
+    cout<<"CUDA availability:";
+    if(torch::cuda::is_available()){
+        cout<<"True"<<endl;
+        cout<<"cuDNN availability:";
+        if(torch::cuda::cudnn_is_available()){
+            cout<<"True"<<endl;
+        }
+        else{
+            cout << "False" << endl;
+        }
+        //print #GPU
+        cout << "This device has "<<torch::cuda::device_count()<<" GPU(s)" << endl;
+    }
+    else{
+        cout << "False" << endl;
+    }  
+    
+    cout << "LibTorch version: "<< TORCH_VERSION_MAJOR<<"."
     <<TORCH_VERSION_MINOR<<"."
     <<TORCH_VERSION_PATCH<<endl;
     return 0;
